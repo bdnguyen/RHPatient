4b63d295c60334e88646b7804ae8baa484ec10fc
BD Nguyen <binhduongng@gmail.com>
2014-12-08 21:38:35 +0100

    fixed orientation reset config for homeActivity

--- src/com/pjwstk/rehapp/SingleExerciseActivity.java
+++ src/com/pjwstk/rehapp/SingleExerciseActivity.java
@@ -160,6 +160,7 @@ public class SingleExerciseActivity extends FragmentActivity {
   	      public void onInit(int status) {
   	         if(status != TextToSpeech.ERROR){
   	        	 ttsObj.setLanguage(new Locale("pl_PL"));
+  	        	 ttsObj.setSpeechRate((float)0.7);
 //  	             ttsObj.setLanguage(Locale.UK);
   	         }				
   	      }
